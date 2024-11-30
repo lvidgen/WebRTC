74b0fecbe2bbab8ce67fa49758bfc63cfffd6324
lvidgen <15856004+lvidgen@users.noreply.github.com>
2024-12-01 10:44:47 +1100

    authentication

--- webrtc.js
+++ webrtc.js
@@ -100,7 +100,7 @@ fetch("https://stunauth.frostyer.com", options)
                }
             ]
         }
-
+});
 	
     // Create own peer object with connection to shared PeerJS server
     
@@ -156,7 +156,7 @@ fetch("https://stunauth.frostyer.com", options)
 		*/
         showLogIns(peer, conn);
     		});
-	});    
+	    
     });
 }
 
